b35063936fc78022d53633a5f9a0b0d8aa9276a8
yuta.onishi <rad6242@icloud.com>
2026-05-04 21:53:56 +0900

    Replace test to spec style.

--- test/test_tescon.rb
+++ test/test_tescon.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require_relative "test_helper"
 
-class TestTescon < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Tescon::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+describe Tescon do
+  it "has a version number" do
+    expect(Tescon::VERSION).must_be_kind_of String
   end
 end
